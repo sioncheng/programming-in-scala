c495a54c05783ef1efe20c6426a0f107b2f093eb
sioncheng <chengsion@hotmail.com>
2015-06-22 17:04:02 +0800

    add match expression

--- ch7/if.scala
+++ ch7/if.scala
@@ -2,3 +2,12 @@
 val arg0 = if (!args.isEmpty) args(0) else "default"
 
 println(arg0)
+
+val friend = arg0 match {
+	case "salt" => "pepper"
+	case "chips" => "salsa"
+	case "eggs" => "bacon"
+	case _ => "huh?"
+}
+
+println(friend)
