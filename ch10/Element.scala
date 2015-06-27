4257c4434603dd3c1f336bd57b3a4a813a4e4105
sioncheng <chengsion@hotmail.com>
2015-06-27 21:58:18 +0800

    class,abstract,extends

---
+++ ch10/Element.scala
@@ -0,0 +1,24 @@
+abstract class Element {
+	def contents : Array[String]
+	val height = contents.length
+	val width = if (height == 0) 0 else contents(0).length
+}
+
+class ArrayElement(conts:Array[String]) extends Element {
+	def contents : Array[String] = conts
+}
+
+class ArrayElement2 (
+	val contents : Array[String]
+) extends Element{}
+
+class LineElement(s : String) extends ArrayElement(Array(s)){
+	//override val height : Int = 1
+	//override val width : Int = s.length
+}
+
+val arr = Array("s1","s22")
+println(arr.length)
+
+val ae = new ArrayElement(arr)
+println(ae.height + "," + ae.width)
