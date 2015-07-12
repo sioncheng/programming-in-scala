d0c2d581d10b281a159c6268da2340ae057ec12a
sioncheng <chengsion@hotmail.com>
2015-07-13 06:58:19 +0800

    constuctor patterns and type patterns

--- ch15/Expr.scala
+++ ch15/Expr.scala
@@ -19,3 +19,61 @@ def simplifyTop(expr:Expr) = expr match {
 val expr = simplifyTop(UnOp("-", UnOp("-",Number(100))))
 
 println(expr == Number(100))
+
+//constant patterns
+def describe(x:Any) = x match {
+	case 1 => "one"
+	case true => "truth"
+	case "hello" => "hi"
+	case Nil => "the emplty list"
+	case _ => "something else"
+}
+
+println(describe(1))
+println(describe("hello"))
+println(describe("hahah"))
+
+//constructor patterns
+
+def list03(x:Any) = x match {
+	case List(0, _, _) => "found"
+	case _ => "what?"
+}
+
+def list0(x:Any) = x match {
+	case List(0, _*) => "found"
+	case _ => "what?"
+}
+
+println(list03(List(0,1,2)))
+println(list03(List(0,1,2,3)))
+println(list0(List(0,1,2,3)))
+
+
+//typed patterns
+def generalSize(x:Any) = x match {
+	case s : String => s.length
+	case m : Map[_,_] => m.size
+	case l : List[_] => l.length
+	case _ => -1
+}
+
+println(generalSize("hahah"))
+println(generalSize(List(1,2,3,4,5)))
+
+//
+def generalSizeFunc(x:Any) = {
+	if (x.isInstanceOf[String]) {
+		val s = x.asInstanceOf[String]
+		s.length
+	} else if (x.isInstanceOf[Map[_,_]]) {
+		val m  = x.asInstanceOf[Map[_,_]]
+		m.size
+	} else if (x.isInstanceOf[List[_]]) {
+		val l = x.asInstanceOf[List[_]]
+		l.length
+	} else {
+		-1
+	}
+}
+println(generalSizeFunc(List(1,2)))
